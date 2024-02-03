0bc6d9b510680463f8699f4e79c7e31ada54d1a4
themanprvnce <princeadigwe29@gmail.com>
2024-02-03 17:25:00 +0100

    cors on the beat

--- prototype/middlewares/cors.js
+++ prototype/middlewares/cors.js
@@ -5,8 +5,9 @@ function cors(req, res, next) {
 	res.setHeader("Access-Control-Allow-Origin", /*using specific url so that cookies will be allowed*/`*`);
 	
 	if(req.method === "OPTIONS") {
+		console.log("got a preflight request");
 		res.setHeader("Access-Control-Allow-Methods",/*using specific kini so cookies will be allowed*/ "POST, GET, PUT");
-		res.setHeader("Access-Control-Allow-Headers", "*");
+		res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		res.setHeader("Access-Control-Max-Age", "900"/*900secs / 15mins*/);
 		res.setHeader('Content-Length', '0');
 	    res.writeHead(204);
